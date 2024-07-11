3d087d467b635ece7413eb404f902ae241cfe32b
Camilo Mezú Mina <camilo.mezu@correounivalle.edu.co>
2024-07-11 14:46:54 -0500

    fix api

--- graphql/client.ts
+++ graphql/client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const gqlClient = new ApolloClient({
-  uri: process.env.BASE_URL, // Replace with your GraphQL endpoint
+  uri: process.env.BASE_URL + '/api/graphql',
   cache: new InMemoryCache(),
 });
